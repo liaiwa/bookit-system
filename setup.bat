bcfdb6e9c09a41fcc3227eb413b569403a2c6bcd
Arlia <miiaiwachn@gmail.com>
2025-12-15 18:40:55 +0800

    hello world

---
+++ setup.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Setting up IMS566/IMS560 projects...
+cd C:\laragon\www
+git clone https://github.com/liaiwa/bookit-system.git
+git clone https://github.com/liaiwa/classio-system.git
+echo Done! Projects are in C:\laragon\www
+pause
